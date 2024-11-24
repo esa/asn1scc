10a9dcffd5488504f956dcbbf6510eed00452a2c
Maxime Perrotin <maxime.perrotin@esa.int>
2024-11-24 22:51:35 +0100

    Update version to 4.5.2.3

--- asn1scc/Program.fs
+++ asn1scc/Program.fs
@@ -123,7 +123,7 @@ let printVersion () =
     //let fvi = System.Diagnostics.FileVersionInfo.GetVersionInfo(assembly.Location);
     //let version = fvi.FileVersion;
 
-    let version = "4.5.2.2"
+    let version = "4.5.2.3"
     printfn "asn1scc version %s\n" version
     ()
 
