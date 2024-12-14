09589fcf3183d370a332e39c13508892dc84328e
George Mamais <gmamais@gmail.com>
2024-12-14 17:26:25 +0000

    fix github-build.sh

--- github-build.sh
+++ github-build.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
 
+# Fix ownership of /app
+chown -R myuser:myuser /app
+
+# Ensure HOME is set correctly for sdkman
+export HOME=/home/myuser
+
+# Source sdkman initialization script
 source "$HOME/.sdkman/bin/sdkman-init.sh"
 
+# Build and test commands
 dotnet build Antlr/
 dotnet build parseStg2/
 dotnet build "asn1scc.sln"
@@ -12,6 +20,6 @@ cd v4Tests || exit 1
 ../regression/bin/Debug/net9.0/regression -l c -ws 8 -s true -p 12 || exit 1
 ../regression/bin/Debug/net9.0/regression -l Ada -ws 8 -s true -p 12 || exit 1
 
-#scala tests
+# Scala tests
 cd ../PUSCScalaTest || exit 1
 dotnet test || exit 1
