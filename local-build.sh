c3a4f0b456806c4f14d7953bc6ad44879f518880
George Mamais <gmamais@gmail.com>
2025-03-16 16:53:31 +0100

    improve local build

--- local-build.sh
+++ local-build.sh
@@ -3,18 +3,26 @@ echo "****"
 echo $1
 echo "****"
 source "$HOME/.sdkman/bin/sdkman-init.sh"
+echo "git config --global --add safe.directory /app/.git"
 git config --global --add safe.directory /app/.git || exit
 cd /workdir/ || exit
+echo "git clone /app/ asn1scc"
 git clone /app/ asn1scc || exit
 cd asn1scc || exit
+echo "git checkout $1"
 git checkout $1 || exit
+echo "git config --global --add safe.directory /app/.git"
 git config --global --add safe.directory /app/.git || exit
+echo "git pull"
 git pull || exit
+echo "git rev-parse --abbrev-ref HEAD"
 git rev-parse --abbrev-ref HEAD || exit
+echo "dotnet build"
 dotnet build Antlr/ || exit 1
 dotnet build parseStg2/ || exit 1
 dotnet build "asn1scc.sln" || exit 1
 cd v4Tests || exit 1
+echo "run local tests"
 ../regression/bin/Debug/net9.0/regression -l c -ws 4 -s false -p 48 || exit 1
 ../regression/bin/Debug/net9.0/regression -l Ada -ws 4 -s false -p 48 || exit 1
 ../regression/bin/Debug/net9.0/regression -l c -ws 8 -s true -p 48 -ig || exit 1
@@ -22,5 +30,6 @@ cd v4Tests || exit 1
 ../regression/bin/Debug/net9.0/regression -l Ada -ws 8 -s true -p 48 || exit 1
 
 #scala tests
+echo "run scala tests"
 cd ../PUSCScalaTest || exit 1
 dotnet test || exit 1
