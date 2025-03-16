17ab612c71941144bb55681a05da990ef3c642e4
George Mamais <gmamais@gmail.com>
2025-03-16 12:14:57 +0200

    Simplify Docker run command in build script
    unin new lines

--- run-local-build-using-docker-image.sh
+++ run-local-build-using-docker-image.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #docker build -f Dockerfile.local.wsl -t asn1scc .
-docker run -ti --rm -v $(pwd):/app -v asn1scc_workdir:/workdir asn1scc bash -c "./local-build.sh $(git rev-parse --abbrev-ref HEAD)"
+docker run -ti --rm -v .:/app -v asn1scc_workdir:/workdir asn1scc bash -c "./local-build.sh $(git rev-parse --abbrev-ref HEAD)"
