8a95a60a136280b66ff651edbc1669cc43e81d45
George Mamais <gmamais@gmail.com>
2025-03-16 14:10:09 +0200

    Add build arguments for conditional non-root user creation
    Updated Dockerfile and Dockerfile.local.wsl to introduce build arguments for user creation, enabling the conditional creation of a non-root user. The default user is set to `root` with a user ID of `0`, which can be overridden during the build process. User context is switched to the specified user after installations. The `run-local-build-using-docker-image.sh` script has also been modified to support these changes.

--- run-local-build-using-docker-image.sh
+++ run-local-build-using-docker-image.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-#docker build -f Dockerfile.local.wsl -t asn1scc .
+#docker build -f Dockerfile.local.wsl --build-arg NON_ROOT_USER=true --build-arg USERNAME=myuser --build-arg USERID=1000 -t myimage .
 docker run -ti --rm -v .:/app -v asn1scc_workdir:/workdir asn1scc bash -c "./local-build.sh $(git rev-parse --abbrev-ref HEAD)"
