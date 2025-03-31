fa21ed0fe12226fd706f9580b3111210b2640f98
Maxime Perrotin <maxime.perrotin@esa.int>
2025-03-31 14:45:15 +0200

    Update version in Program.fs
    Upgrade to 4.6.0.0 as the new feature to reduce the amount of generated code is major

--- asn1scc/Program.fs
+++ asn1scc/Program.fs
@@ -124,7 +124,7 @@ let printVersion () =
     //let fvi = System.Diagnostics.FileVersionInfo.GetVersionInfo(assembly.Location);
     //let version = fvi.FileVersion;
 
-    let version = "4.5.2.9"
+    let version = "4.6.0.0"
     printfn "asn1scc version %s\n" version
     ()
 
