7ebd0079b29226fcdd7444cf2926e4436954c92b
Maxime Perrotin <maxime.perrotin@esa.int>
2025-04-15 12:38:30 +0000

    Update the script that creates a .deb file for asn1scc

--- contrib/prepare_debian_package.sh
+++ contrib/prepare_debian_package.sh
@@ -11,36 +11,35 @@ INITIAL_FOLDER=$(pwd)
 SHARE_FOLDER=$INSTALL_ROOT/share/asn1scc
 BIN_FOLDER=$INSTALL_ROOT/bin
 cd ..
-VERSION=3.2.$(cat Asn1f2/SvnVersion.cs | cut -d \" -f 2)
-echo Creating a Debian package for ASN1Scc version $VERSION
-test -f Asn1f2/bin/Debug/Asn1f2.exe || (echo 'Run ./build.sh first to build ASN1SCC' && false)
-rm -rf asn1scc
-mkdir -p asn1scc/DEBIAN
-mkdir -p asn1scc/$BIN_FOLDER
-mkdir -p asn1scc/$SHARE_FOLDER
-cp -r Asn1f2/bin/Debug/* asn1scc/$SHARE_FOLDER/
-rm -f asn1scc/$SHARE_FOLDER/*.mdb
-mv asn1scc/$SHARE_FOLDER/Asn1f2.exe asn1scc/$SHARE_FOLDER/asn1.exe
-cd asn1scc/$BIN_FOLDER
-ln -s ../share/asn1scc/* .
+BINARY_PATH=./asn1scc/bin/Release/net7.0/linux-x64/publish
+VERSION="$(${BINARY_PATH}/asn1scc -v | head -1 | awk '{print $NF}')"
+echo "[-] Creating a Debian package for ASN1SCC version ${VERSION}"
+test -f ${BINARY_PATH}/asn1scc || (echo 'You must first build ASN1SCC (make -C .. -f Makefile.debian publish)' && false)
+rm -rf asn1scc_deb
+mkdir -p asn1scc_deb/DEBIAN
+mkdir -p asn1scc_deb/$BIN_FOLDER
+mkdir -p asn1scc_deb/$SHARE_FOLDER
+cp -r ${BINARY_PATH}/* asn1scc_deb/$SHARE_FOLDER/
+cd asn1scc_deb/$BIN_FOLDER
+ln -s ../share/asn1scc/asn1scc .
 cd $INITIAL_FOLDER/..
 echo "Package: asn1scc
 Version: ${VERSION}
 Section: base
 Priority: optional
 Architecture: all
-Depends: libmono-system-runtime4.0-cil,libmono-corlib4.0-cil,libmono-system-runtime-serialization-formatters-soap4.0-cil, libmono-system-web4.0-cil,libmono-system-xml4.0-cil,libmono-system4.0-cil,mono-runtime,libmono-system-numerics4.0-cil,libmono-system-data-linq4.0-cil,libmono-corlib2.0-cil,libmono-system2.0-cil,libmono-corlib4.5-cil
-Maintainer: Thanassis Tsiodras
-Description: ASN.1 Space Certified Compiler - Generate Spark/Ada and C code for ASN.1 uPER or custom binary encoders/decoders
-Homepage: http://github.com/maxime-esa/asn1scc" > asn1scc/DEBIAN/control
+Depends: 
+Maintainer: George Mamais
+Description: ASN.1 Space Certified Compiler - Generate Spark/Ada/Scala/C code for ASN.1 uPER or custom binary encoders/decoders
+Homepage: http://github.com/esa/asn1scc" > asn1scc_deb/DEBIAN/control
 echo '#!/bin/sh
 echo "All done!"
-' >> asn1scc/DEBIAN/postinst
-chmod +x asn1scc/DEBIAN/postinst
-dpkg-deb --build asn1scc
+' >> asn1scc_deb/DEBIAN/postinst
+chmod +x asn1scc_deb/DEBIAN/postinst
+dpkg-deb --build asn1scc_deb
 cd $INITIAL_FOLDER
 mv ../*.deb .
-rm -rf ../asn1scc
-echo -e 'Done...\n\nThe .deb file is here:\n'
+rm -rf ../asn1scc_deb
+echo -e '[-] Done...\n\nThe .deb file is here:\n'
 pwd
 ls -l *deb
