f1421b0506dd0846fb932cd5749e5ba7144b075e
George Mamais <gmamais@gmail.com>
2025-05-04 20:52:48 +0300

    dos2unix

--- run-local-build-using-docker-image.sh
+++ run-local-build-using-docker-image.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-docker build --build-arg NON_ROOT_USER=true --build-arg USERNAME=myuser --build-arg USERID=1000 -t myimage .
+docker build --build-arg NON_ROOT_USER=true --build-arg USERNAME=myuser --build-arg USERID=1000 -t asn1scc .
 docker volume rm asn1scc_workdir
 docker volume create asn1scc_workdir
 docker run -ti --rm -v .:/app -v asn1scc_workdir:/workdir asn1scc bash -c "./local-build.sh $(git rev-parse --abbrev-ref HEAD)"
