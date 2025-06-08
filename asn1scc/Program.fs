8e52ebd2b7fd8ffe76671dd841ee44f779364432
George Mamais <gmamais@gmail.com>
2025-06-08 13:20:56 +0300

    #341

--- asn1scc/Program.fs
+++ asn1scc/Program.fs
@@ -124,7 +124,7 @@ let printVersion () =
     //let fvi = System.Diagnostics.FileVersionInfo.GetVersionInfo(assembly.Location);
     //let version = fvi.FileVersion;
 
-    let version = "4.6.0.5"
+    let version = "4.6.0.6"
     printfn "asn1scc version %s\n" version
     ()
 
