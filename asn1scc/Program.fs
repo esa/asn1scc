a224fdb14e16d8bfc5a63986c0fe4378d956bf5c
George Mamais <gmamais@gmail.com>
2025-09-13 22:41:26 +0300

    Update version number to 4.6.0.11

--- asn1scc/Program.fs
+++ asn1scc/Program.fs
@@ -124,7 +124,7 @@ let printVersion () =
     //let fvi = System.Diagnostics.FileVersionInfo.GetVersionInfo(assembly.Location);
     //let version = fvi.FileVersion;
 
-    let version = "4.6.0.10"
+    let version = "4.6.0.11"
     printfn "asn1scc version %s\n" version
     ()
 
