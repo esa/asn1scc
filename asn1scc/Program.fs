75cf6159b99a6e9ebb1f9ddcc1aed8dd6777e21d
George Mamais <gmamais@gmail.com>
2026-03-22 14:02:50 +0200

    Update version to 4.7.0.2

--- asn1scc/Program.fs
+++ asn1scc/Program.fs
@@ -126,7 +126,7 @@ let printVersion () =
     //let fvi = System.Diagnostics.FileVersionInfo.GetVersionInfo(assembly.Location);
     //let version = fvi.FileVersion;
 
-    let version = "4.7.0.1"
+    let version = "4.7.0.2"
     printfn "asn1scc version %s\n" version
     ()
 
