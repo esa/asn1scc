9bc6a3d1ad3f77232ad9dd9e2ebd23c749367bc9
Maxime Perrotin <maxime.perrotin@esa.int>
2026-09-11 12:53:00 +0000

    C backend seems to work

--- BackendAst/DAstVariables.fs
+++ BackendAst/DAstVariables.fs
@@ -53,7 +53,7 @@ let convertStringValue2TargetLangStringLiteral (lm:LanguageMacros) mxSizeUper (v
         parts |>
         List.collect(fun s ->
             match s with
-            | CStringValue  sv -> sv |> Seq.map(fun ch -> lm.vars.PrintCharValue ch) |> Seq.toList
+            | CStringValue  sv -> sv |> Seq.map(fun ch -> lm.vars.PrintStringChar ch) |> Seq.toList
             | SpecialCharacter  CarriageReturn -> [lm.vars.PrintCR ()]
             | SpecialCharacter  LineFeed       -> [lm.vars.PrintLF ()]
             | SpecialCharacter  HorizontalTab  -> [lm.vars.PrintHT ()]
